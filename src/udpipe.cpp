364469f4e87c8fea0c2d3d9c014ed4856b71ff84
Jan Wijffels <jwijffels@bnosac.be>
2017-09-13 16:25:16 +0000

    version 0.1.1

--- src/udpipe.cpp
+++ src/udpipe.cpp
@@ -19766,7 +19766,8 @@ void multiword_splitter::append_token(string_piece token, string_piece misc, sen
   }
 
   // Determine casing
-  enum { UC_FIRST, UC_ALL, OTHER } casing = OTHER;
+  enum { UC_FIRST, UC_ALL, OTHER }; 
+  int casing = OTHER;
 
   if (unicode::category(utf8::first(token.str, token.len)) & unicode::Lut) {
     casing = UC_ALL;
